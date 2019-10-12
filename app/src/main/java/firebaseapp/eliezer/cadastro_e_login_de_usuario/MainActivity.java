9d6bbd294d8df00aa93594408c565a85823c872e
Eliezer Fernando António <47981638+eliezerantonio@users.noreply.github.com>
2019-10-12 22:49:49 +0100

    Introducao

---
+++ app/src/main/java/firebaseapp/eliezer/cadastro_e_login_de_usuario/MainActivity.java
@@ -0,0 +1,14 @@
+package firebaseapp.eliezer.cadastro_e_login_de_usuario;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/firebaseapp/eliezer/cadastro_e_login_de_usuario/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package firebaseapp.eliezer.cadastro_e_login_de_usuario;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
